4503bf60bd013fff3592bc091dcfbfc9c5cbc215
Maia Johnson <maiaj1306@gmail.com>
2022-10-14 00:06:10 -0500

    created POST request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,8 +5,32 @@ $(document).ready(onReady);
 function onReady() {
     console.log('jquery ready');
 
+    $('#addButton').on('click', handleSubmit);
+
     getTasks();
 };
+
+function handleSubmit() {
+    console.log('submit button clicked');
+    let task = {};
+    task.name = $('#taskIn').val();
+    addTask(task);
+};
+
+function addTask(newTask) {
+    $.ajax({
+        type: "POST",
+        url: '/todo',
+        data: newTask
+    })
+        .then(function(response) {
+            console.log('response from server', response);
+            getTasks();
+        })
+        .catch(function(error) {
+            console.log('error in POST', error);
+        })
+}
 
 function getTasks() {
     console.log('in getTasks');
--- server/routes/to.do.router.js
+++ server/routes/to.do.router.js
@@ -17,9 +17,24 @@ toDoRouter.get('/', (req, res) => {
 });
 
 // POST
+toDoRouter.post('/', (req, res) => {
+    let newToDo = req.body;
 
+    let sqlText = `INSERT INTO "todo" ("task_name")
+                    VALUES $1;`;
+    pool.query(sqlText, [newToDo])
+        .then(result => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log('error adding new task', err);
+            res.sendStatus(500);
+        });
+});
 
 // PUT
 
 
 // DELETE
+
+module.exports = toDoRouter;
